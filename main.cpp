f51f2598b8f869105f2ea115ec44ffc0d48b1262
karl <karl.bittner@hexaquo.at>
2019-10-19 23:58:39 +0200

    Overloaded +, +=, char conversion

--- main.cpp
+++ main.cpp
@@ -22,6 +22,14 @@ public:
     /// Move assignment operator
     String &operator=(String &&other);
 
+    /// += operators for concatenate
+    /// Used in the + operator as well (defined outside the class)
+    String &operator+=(const String &other);
+    String &operator+=(const char *other);
+
+    /// Conversion to char pointer
+    operator const char *() const;
+
     ~String();
 
     /// Append a String at the end of this String.
@@ -64,9 +72,6 @@ String &String::operator=(const String &other) {
         // Delete old text and create new
         delete[] text;
 
-        // TODO: If this throws an out of memory exception, the length is incorrect. Can we ignore this since
-        //  if that happens, things break anyways? We need another temporary pointer otherwise...
-        //  Or copy-and-swap could be used
         text = new char[length + sizeof(char)];
 
         // Copy text
@@ -93,6 +98,39 @@ String &String::operator=(String &&other) {
     }
 
     return *this;
+}
+
+String &String::operator+=(const String &other) {
+    concatenate(other);
+    return *this;
+}
+
+String &String::operator+=(const char *other) {
+    concatenate(other);
+    return *this;
+}
+
+String operator+(String left, const String &right) {
+    left += right;
+    return left;
+}
+
+String operator+(String left, const char *right) {
+    left += right;
+    return left;
+}
+
+// TODO: Should cases like `"Hello" + String("World")` be handled?
+//  I think not; it would be up to `const char *` to handle that, since getting a String after adding something
+//  to a `const char *` is strange. It could look like this however:
+//String operator+(const char *left, const String &right) {
+//    String left_string = String(left);
+//    left_string += right;
+//    return left_string;
+//}
+
+String::operator const char *() const {
+    return this->c_str();
 }
 
 String::~String() {
@@ -132,48 +170,30 @@ int main() {
     std::cout << "Test output: |" << testString.c_str() << "|" << std::endl;
     std::cout << "Length: " << testString.getLength() << std::endl;
 
-    testString.concatenate("+1");
+    String added = testString + String(" again!");
 
-    std::cout << "Test output: |" << testString.c_str() << "|" << std::endl;
-    std::cout << "Length: " << testString.getLength() << std::endl;
+    std::cout << "Test output: |" << added << "|" << std::endl;
+    std::cout << "Length: " << added.getLength() << std::endl;
 
-    String testStringCopy(testString);
+    added += "+1";
 
-    std::cout << "Copied string: |" << testStringCopy.c_str() << "|" << std::endl;
-    std::cout << "Length: " << testStringCopy.getLength() << std::endl;
+    std::cout << "Test output: |" << added << "|" << std::endl;
+    std::cout << "Length: " << added.getLength() << std::endl;
 
-    String otherTextString;
+    added += String("+2");
 
-    std::cout << "New empty string: |" << otherTextString.c_str() << "|" << std::endl;
-    std::cout << "Length: " << otherTextString.getLength() << std::endl;
+    std::cout << "Test output: |" << added << "|" << std::endl;
+    std::cout << "Length: " << added.getLength() << std::endl;
 
-    otherTextString = testStringCopy;
+    const String addedConst = added + "+3";
 
-    std::cout << "Copy assigned string: |" << otherTextString.c_str() << "|" << std::endl;
-    std::cout << "Length: " << otherTextString.getLength() << std::endl;
+    std::cout << "Test output: |" << addedConst << "|" << std::endl;
+    std::cout << "Length: " << addedConst.getLength() << std::endl;
 
-    otherTextString.concatenate("Additional Text");
+    const String total = added + addedConst;
 
-    std::cout << "Copy assigned string: |" << otherTextString.c_str() << "|" << std::endl;
-    std::cout << "Length: " << otherTextString.getLength() << std::endl;
-
-    std::cout << "Unchanged previous string: |" << testStringCopy.c_str() << "|" << std::endl;
-    std::cout << "Length: " << testStringCopy.getLength() << std::endl;
-
-    String moved(std::move(otherTextString));
-
-    std::cout << "Moved string: |" << moved.c_str() << "|" << std::endl;
-    std::cout << "Length: " << moved.getLength() << std::endl;
-
-    otherTextString = String("Recycling");
-
-    std::cout << "Old reused string: |" << otherTextString.c_str() << "|" << std::endl;
-    std::cout << "Length: " << otherTextString.getLength() << std::endl;
-
-    otherTextString = std::move(testString);
-
-    std::cout << "Move assigned: |" << otherTextString.c_str() << "|" << std::endl;
-    std::cout << "Length: " << otherTextString.getLength() << std::endl;
+    std::cout << "Test output: |" << total << "|" << std::endl;
+    std::cout << "Length: " << total.getLength() << std::endl;
 
     return 0;
 }
